93a5012795194ebaf0124feb240094cb1c67c0a0
Myron <myron@pop-os>
2024-05-16 16:39:44 +0200

    Added dropdown for enable disable tranlation per scope.

--- Model/Config.php
+++ Model/Config.php
@@ -149,4 +149,11 @@ class Config
             self::XML_PATH_OPENAI_API_ADVANCED_TRANSLATE_OUTPUT
         );
     }
+
+    public function getEnableTranslateOutput(): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_OPENAI_API_ADVANCED_TRANSLATE_OUTPUT
+        );
+    }
 }
